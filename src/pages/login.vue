2e30ef3f3f6a118f49826269767cb3b99f552829
terashima-hiroshi <terashima@griffin-net.co.jp>
2022-04-19 11:08:11 +0900

    initial commit

---
+++ src/pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input v-model="state.username" />
+    <button @click="login">login</button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface State {
+  username: string;
+}
+
+export default defineComponent({
+  setup(props, context) {
+    const state = reactive<State>({
+      username: "initial value",
+    });
+
+    const login = () => {
+      console.log("login", state.username);
+    };
+
+    return {
+      state,
+      login,
+    };
+  },
+});
+</script>
