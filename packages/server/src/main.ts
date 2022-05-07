b320c086341cb6685db9a58dcda9d28ca0e73300
teracy164 <literacy1945@gmail.com>
2022-05-07 15:18:39 +0900

    サーバーの起動ポートを環境変数から設定できるように修正

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup(prefix, app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
