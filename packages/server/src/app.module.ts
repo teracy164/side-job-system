0a8bf14a316b2dfec67ecfa8a79f80016e12c587
teracy164 <literacy1945@gmail.com>
2022-05-07 15:38:28 +0900

    DBのSSL接続設定の修正

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -4,6 +4,17 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { SequelizeModule } from '@nestjs/sequelize';
 
+console.log('host', process.env.DATABASE_HOST || 'localhost');
+console.log('db', process.env.DATABASE_NAME || 'test');
+console.log('user', process.env.DATABASE_USER || 'root');
+console.log('password', process.env.DATABASE_PASSWORD || 'root');
+console.log('db ssl:', Boolean(process.env.DATABASE_SSL || false));
+
+const dialectOptions: { ssl: boolean | any } = { ssl: false };
+if (process.env.DATABASE_SSL) {
+  dialectOptions.ssl = { require: true, rejectUnauthorized: false };
+}
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
@@ -18,6 +29,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
       username: process.env.DATABASE_USER || 'root',
       password: process.env.DATABASE_PASSWORD || 'root',
       ssl: Boolean(process.env.DATABASE_SSL || false),
+      dialectOptions,
       autoLoadModels: true,
       synchronize: true,
       sync: {
